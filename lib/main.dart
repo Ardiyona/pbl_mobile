4b3b60367b82e22a357c385b40b952ed9c86bcc1
Dhiya Rakha Ardiyona <rakhaardiyona98@gmail.com>
2024-09-25 20:46:50 +0700

    List Notifikasi *added comments

--- lib/main.dart
+++ lib/main.dart
@@ -11,48 +11,51 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
-      appBar: AppBar(
-        title: const Row(children: [
-          Icon(
-            Icons.arrow_back_ios,
-            color: Colors.white,
-            size: 20.0,
+          appBar: AppBar(
+            // Title dan Tombol Back
+            title: const Row(children: [
+              Icon(
+                Icons.arrow_back_ios,
+                color: Colors.white,
+                size: 20.0,
+              ),
+              Text(
+                'Notifikasi',
+                style: TextStyle(
+                  fontFamily: 'Poppins',
+                  fontSize: 20.0,
+                  color: Colors.white,
+                ),
+              ),
+            ]),
+            backgroundColor: const Color.fromARGB(255, 239, 84, 40),
           ),
-          Text(
-            'Notifikasi',
-            style: TextStyle(
-              fontFamily: 'Poppins',
-              fontSize: 20.0,
+          // Memanggil Widget MyAppBody yang isinya body dari page
+          body: MyAppBody(),
+          // Navigasi bawah dengan icon2
+          bottomNavigationBar: BottomNavigationBar(items: const[
+              BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+              BottomNavigationBarItem(icon: Icon(Icons.menu_book), label: 'Repository'),
+              BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
+            ],
+            selectedItemColor: const Color.fromARGB(255, 239, 84, 40),
+          ),
+          /* floatingActionButton: FloatingActionButton(
+            onPressed: () => 0,
+            backgroundColor: const Color.fromARGB(255, 239, 84, 40),
+            shape: const CircleBorder(),
+            child: const Icon(
+              Icons.add,
               color: Colors.white,
             ),
           ),
-        ]),
-        backgroundColor: const Color.fromARGB(255, 239, 84, 40),
-      ),
-      body: MyAppBody(),
-      //bottomNavigationBar: const MyAppBottomNav(),
-      bottomNavigationBar: BottomNavigationBar(items: const[
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
-          BottomNavigationBarItem(icon: Icon(Icons.menu_book), label: 'Repository'),
-          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
-        ],
-        selectedItemColor: const Color.fromARGB(255, 239, 84, 40),
-      ),
-      /* floatingActionButton: FloatingActionButton(
-        onPressed: () => 0,
-        backgroundColor: const Color.fromARGB(255, 239, 84, 40),
-        shape: const CircleBorder(),
-        child: const Icon(
-          Icons.add,
-          color: Colors.white,
-        ),
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked, */
+          floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked, */
     )
     );
   }
 }
 
+// Widget untuk body
 class MyAppBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -60,6 +63,7 @@ class MyAppBody extends StatelessWidget {
       body: Container(
         child: ListView(
           children: [
+            // list untuk notifikasi sing tersedia
             ListTile(
               leading: Icon(Icons.library_books, size: 35.0,),
               subtitle: Text("Intelligent Manufacturing: Internet of Things (IoT), Artificial Intelligence, Digital Transformation into Industry 4.0"),
